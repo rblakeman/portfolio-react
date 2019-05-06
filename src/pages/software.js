1e4bcb142bac294259c6e804f9ac8550c12f3922
Ryan Blakeman <rblakeman31@gmail.com>
2019-05-05 17:57:11 -0700

    Added icons and updated descriptions on software projects

--- src/pages/software.js
+++ src/pages/software.js
@@ -23,6 +23,19 @@ import DiagramIMG from '../img/software/clamshell/process_diagram.png'
 
 import Banner from '../components/banner'
 import SoftwareProject from '../components/software_project'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faReact,
+  faJs,
+  faHtml5,
+  faPhp,
+  faCss3Alt,
+  faAndroid,
+  faJava,
+  faRaspberryPi
+} from '@fortawesome/free-brands-svg-icons'
+import { faDatabase, faMicrochip } from '@fortawesome/free-solid-svg-icons'
+
 const styles = {
   container: {
     display: 'flex',
@@ -65,6 +78,8 @@ const styles = {
   }
 }
 
+const FA_SIZE = '5x'
+
 export default class Software extends Component {
   render() {
     return (
@@ -79,11 +94,33 @@ export default class Software extends Component {
             { text: 'Weather', img: WeatherIMG },
             { text: 'YouTube', img: YouTubeIMG }
           ]}
+          icons={[
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faReact}
+                  style={{ color: 'rgb(100,212,242)', marginLeft: '10px' }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            },
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faJs}
+                  style={{ color: 'rgb(246,222,56)', marginLeft: '10px' }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            }
+          ]}
         />
         <SoftwareProject
           title="Web Development Catalog"
           tags="HTML, PHP, JS, CSS"
-          contents="GitHub Repository from all of my projects and assignments from university course. Screenshots are from final project for course."
+          contents="GitHub Repository from all of my projects and assignments from university course. Screenshots are from final project, demonstrating the basic CRUD operations on the database."
           images={[
             { text: 'Home', img: HomeWebIMG },
             { text: 'LogIn', img: LogInIMG },
@@ -91,17 +128,89 @@ export default class Software extends Component {
             { text: 'Add', img: AddIMG },
             { text: 'Updated', img: UpdatedIMG }
           ]}
+          icons={[
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faHtml5}
+                  style={{ color: 'rgb(226,77,44)', marginLeft: '10px' }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            },
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faPhp}
+                  style={{
+                    backgroundColor: 'rgb(85,86,133)',
+                    color: 'white',
+                    marginLeft: '10px',
+                    borderRadius: '10px'
+                  }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            },
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faJs}
+                  style={{ color: 'rgb(246,222,56)', marginLeft: '10px' }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            },
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faCss3Alt}
+                  style={{ color: 'rgb(16,113,181)', marginLeft: '10px' }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            }
+          ]}
         />
         <SoftwareProject
           title="Otter Flight Reservation System"
           tags="Android App"
-          contents="App allows customers of an airline company to create accounts and manage reservations Demonstrated use-case scenarios for both users and administrators to manage SQLite database Created with Android Studio and applied Java focused design skills"
+          contents="App allows customers of an airline company to create accounts and manage reservations. Demonstrated use-case scenarios for both users and administrators to manage SQLite database. Created with Android Studio and applied Java focused design skills"
           images={[
             { text: 'Home', img: HomeAndroidIMG },
             { text: 'Create Account', img: CreateAccountSuccessIMG },
             { text: 'Create Account (fail)', img: CreateAccountFailIMG },
             { text: 'Reserve', img: ReserveIMG },
             { text: 'Cancel Reservation', img: CancelReservationIMG }
+          ]}
+          icons={[
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faAndroid}
+                  style={{ color: 'rgb(103,174,74)', marginLeft: '10px' }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            },
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faJava}
+                  style={{
+                    color: 'rgb(84,131,161)',
+                    marginLeft: '10px'
+                  }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            }
           ]}
         />
         <SoftwareProject
@@ -112,6 +221,47 @@ export default class Software extends Component {
             { text: 'Top', img: TopViewIMG },
             { text: 'Bottom', img: BottomViewIMG },
             { text: 'Diagram', img: DiagramIMG }
+          ]}
+          icons={[
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faDatabase}
+                  style={{
+                    color: 'grey',
+                    marginLeft: '10px'
+                  }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            },
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faMicrochip}
+                  style={{
+                    color: 'rgb(18,77,58)',
+                    marginLeft: '10px'
+                  }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            },
+            {
+              obj: (
+                <FontAwesomeIcon
+                  icon={faRaspberryPi}
+                  style={{
+                    color: 'rgb(194,30,75)',
+                    marginLeft: '10px'
+                  }}
+                  size={FA_SIZE}
+                />
+              ),
+              type: 'fa'
+            }
           ]}
         />
         <Divider variant="middle" style={{ marginTop: 50, marginBottom: 50 }} />
