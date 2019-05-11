16bd3caa88bb4743a5d239a8d6253f7940a0fcc1
Ryan Blakeman <rblakeman31@gmail.com>
2019-05-10 20:35:49 -0700

    Removed 'back to top' npm-package with custom buttton

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,31 +1,90 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import ScrollToTop from 'react-scroll-up'
+import { Button } from '@material-ui/core'
 import KeyboardArrowUpRounded from '@material-ui/icons/KeyboardArrowUpRounded'
+
 const styles = {
   footer: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'left',
+    justifyContent: 'space-between',
     margin: 32,
     marginTop: 0,
     padding: 16,
     paddingTop: 0
+  },
+  scrollButton: {
+    // width: '64px',
+    height: '64px',
+    bottom: '30px',
+    right: '30px',
+    padding: '10px',
+    position: 'fixed',
+    borderRadius: '100%',
+    backgroundColor: '#FFFFFF',
+    color: '#0275D8',
+    boxShadow:
+      '0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12)'
   }
 }
 
-export default (props) => {
-  return (
-    <div style={styles.footer}>
-      <ScrollToTop showUnder={160}>
-        <KeyboardArrowUpRounded />
-      </ScrollToTop>
-      <p style={{ margin: 0 }}>
-        &copy; 2018 Ryan Blakeman &middot;
-        <a href="mailto:rblakeman31@gmail.com">Email</a> &middot;
-        <a href="https://www.linkedin.com/in/rblakeman/">LinkedIn</a>
-        <br />
-      </p>
-    </div>
-  )
+export default class Footer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hidden: true
+    }
+  }
+
+  // componentDidMount() {
+  //   window.onscroll = () => {
+  //     console.log(window.pageYOffset)
+  //     if (window.pageYOffset > 160) {
+  //       this.setState({ hidden: false })
+  //     } else {
+  //       this.setState({ hidden: true })
+  //     }
+  //   }
+  // }
+
+  // componentWillUnmount() {
+  //   window.onscroll = null
+  // }
+
+  render() {
+    return (
+      <div style={styles.footer}>
+        <p style={{ margin: 0 }}>
+          &copy; 2018 Ryan Blakeman &middot;
+          <a href="mailto:rblakeman31@gmail.com">Email</a> &middot;
+          <a href="https://www.linkedin.com/in/rblakeman/">LinkedIn</a>
+          <br />
+        </p>
+        {this.state.hidden ? (
+          <Button
+            style={{
+              ...styles.scrollButton,
+              visiblity: 'hidden'
+            }}
+            onClick={() => {
+              window.scrollTo(0, 0)
+            }}
+          >
+            <KeyboardArrowUpRounded style={{ height: '36px', width: '36px' }} />
+          </Button>
+        ) : (
+          <Button
+            style={styles.scrollButton}
+            onClick={() => {
+              window.scrollTo(0, 0)
+            }}
+          >
+            <KeyboardArrowUpRounded style={{ height: '36px', width: '36px' }} />
+          </Button>
+        )}
+      </div>
+    )
+  }
 }
